f0795ea0d9a8314ebc5c2bf67fd17b85bac3c590
NaderMoa <nadermoazezi@gmail.com>
2024-02-03 22:11:06 -0500

    SQL query completed

---
+++ Starter_Files/Resources/Select_order_query.sql
@@ -0,0 +1,9 @@
+Select * from category
+----------------------
+select * from subcategory limit 10
+-----------------------------------
+select * from contact limit 10
+------------------------------
+select * from campaign limit 10
+-------------------------------
+select * from campaign order by cf_id desc limit 10
